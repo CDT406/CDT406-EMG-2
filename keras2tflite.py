40d57cc1e56cbfd526e16397f6d344aa760e19fd
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-04-30 10:13:11 +0200

    Powerful experimental converter

--- keras2tflite.py
+++ keras2tflite.py
@@ -14,6 +14,8 @@ model = tf.keras.models.load_model(keras_model_path)
 
 # Convert the model.
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.experimental_new_converter = True
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
 
 # Save the model.
