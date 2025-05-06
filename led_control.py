310302684dddc1c29c6bbf9a616e9882957ee0a3
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-06 11:44:12 +0200

    Simpel led kontroll

---
+++ led_control.py
@@ -0,0 +1,31 @@
+import time
+
+
+class LedControl:
+    def __init__(self):
+        self.LED_count = 4
+        self.LED_path = '/sys/class/leds/beaglebone:green:usr'
+        self.leds = []
+
+        # Turn off triggers
+        for i in range(self.LED_count):
+            with open(self.LED_path + str(i) + "/trigger", "w") as f:
+                f.write("none")
+
+        # Open a file for each led
+        for i in range(self.LED_count):
+            self.leds.append(open(self.LED_path + str(i) + "/brightness", "w"))
+
+    def setLED(self, led=0, state=False):
+        val = "1" if state else "0"
+        self.leds[led].seek(0)
+        self.leds[led].write(val)
+        self.leds[led].flush()
+
+    def __del__(self):
+        for led in self.leds:
+            led.close()
+
+if __name__ == '__main__':
+    led_c = LedControl()
+    led_c.setLED(state=True)
