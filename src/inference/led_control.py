b4efbb92318718c6a741e5ebc6aca4a8647b96b2
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-07 09:25:30 +0000

    Update led_control.py

--- src/inference/led_control.py
+++ src/inference/led_control.py
@@ -1,4 +1,5 @@
 import time
+import os.path
 
 
 class LedControl:
@@ -6,42 +7,42 @@ class LedControl:
         self.LED_count = 4
         self.LED_path = '/sys/class/leds/beaglebone:green:usr'
         self.leds = []
+        self.is_on_board = os.path.exists(self.LED_path)
+        
+        if (self.is_on_board):        
+            # Turn off triggers
+            for i in range(self.LED_count):
+                with open(self.LED_path + str(i) + "/trigger", "w") as f:
+                    f.write("none")
 
-        # Turn off triggers
-        for i in range(self.LED_count):
-            with open(self.LED_path + str(i) + "/trigger", "w") as f:
-                f.write("none")
+            # Open a file for each led
+            for i in range(self.LED_count):
+                self.leds.append(open(self.LED_path + str(i) + "/brightness", "w"))
+        else:
+            self.current_state = [False] * self.LED_count
+            
 
-        # Open a file for each led
-        for i in range(self.LED_count):
-            self.leds.append(open(self.LED_path + str(i) + "/brightness", "w"))
-
+    # Write to LED if on board, else print to terminal
     def setLED(self, led=0, state=False):
-        val = "1" if state else "0"
-        self.leds[led].seek(0)
-        self.leds[led].write(val)
-        self.leds[led].flush()            
+        if (self.is_on_board):  
+            val = "1" if state else "0"
+            self.leds[led].seek(0)
+            self.leds[led].write(val)
+            self.leds[led].flush()    
+        else:
+            self.current_state[led] = state
+            print(self.current_state, end="\r", flush=True)   
     
 
     def __del__(self):
-        for led in self.leds:
-            led.close()
-
-
-class TestLedControl:
-    def __init__(self):
-        self.LED_count = 4
-        self.current_state = [False] * self.LED_count
-        
-
-    def setLED(self, led=0, state=False):
-        self.current_state[led] = state
-        print(self.current_state, end="\r", flush=True)
+        if (self.is_on_board):
+            for led in self.leds:
+                led.close()
 
 
 if __name__ == '__main__':
-    # led_c = LedControl()
-    led_c = TestLedControl()
+    led_c = LedControl()
+    
     led_c.setLED(state=True)
     time.sleep(3)
     led_c.setLED(state=False)
