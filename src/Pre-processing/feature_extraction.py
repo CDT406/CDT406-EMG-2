fb8d945a099832d3ab49c09e83ae3df6a2143b7e
Ludwig Bogsveen <lbn22008@student.mdu.se>
2025-05-07 11:08:41 +0200

    update

--- src/Pre-processing/feature_extraction.py
+++ src/Pre-processing/feature_extraction.py
@@ -13,10 +13,15 @@ def compute_mavs(window):
     half = len(window) // 2
     return np.abs(compute_mav(window[:half]) - compute_mav(window[half:]))
 
-def extract_features(window, wamp_threshold=0.02):
-    return [
-        compute_mav(window),
-        compute_wl(window),
-        compute_wamp(window, threshold=wamp_threshold),
-        compute_mavs(window)
-    ]
+def extract_features(window, features = ['mav', 'wl', 'wamp', 'mavs'], wamp_threshold=0.02):
+    extracted_features = []
+    for feature in features:
+        if feature == 'mav':
+            extracted_features.append(compute_mav(window))
+        elif feature == 'wl':
+            extracted_features.append(compute_wl(window))
+        elif feature == 'wamp':
+            extracted_features.append(compute_wamp(window, threshold=wamp_threshold))
+        elif feature == 'mavs':
+            extracted_features.append(compute_mavs(window))
+    return extracted_features
