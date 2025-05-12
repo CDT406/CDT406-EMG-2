75accd7e499b9d4350dcd22fdab374d2f5b04cb8
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-12 11:27:24 +0200

    Update main.py

--- src/inference/main.py
+++ src/inference/main.py
@@ -2,10 +2,15 @@ from led_control import LedControl
 
 
 
-
 if __name__ == '__main__':
     led_control = LedControl()
+    # data_load = DataLoader()
+    # model = ModelLoader()
     
     
-    while 1:
+    # while 1:
+        # window = DataLoader.get_window()
+        # model.set_input_window(window)
+        # output_state = model.get_output_state()
+        # led_control.setState(output_state)
         
