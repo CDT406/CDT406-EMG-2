e46439e7ae65ddf8137172405544980397a3e90b
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-13 11:16:50 +0200

    Update main.py

--- src/inference/main.py
+++ src/inference/main.py
@@ -1,18 +1,23 @@
 from led_control import LedControl
-from analogInput import SensorInput
-from data_process import DataLoader
+from analog_input import SensorInput
+from data_process import DataProcess
+from config import Config
 
 
 
 if __name__ == '__main__':
+    config = Config()
     led_control = LedControl()
-    analog_input = SensorInput()
-    data_process = DataLoader()
+    data_input = SensorInput(frequency=config.frequency, window_size=config.window_size)
+    data_process = DataProcess(config=config, data_input=data_input)
     # model = ModelLoader()
     
     
-    # while 1:
-        # window = DataLoader.get_window()
+    while 1:
+        if (not data_input.has_next()):
+            continue
+            
+        window = data_process.get_window()    
         # model.set_input_window(window)
         # output_state = model.get_output_state()
         # led_control.setState(output_state)
