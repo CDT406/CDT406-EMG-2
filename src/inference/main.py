52b1c845275f400082e0fa070d575d007dce5bb2
Ludwig Bogsveen <lbn22008@student.mdu.se>
2025-05-15 11:25:43 +0200

    update

--- src/inference/main.py
+++ src/inference/main.py
@@ -2,7 +2,7 @@ from led_control import LedControl
 from analog_input import SensorInput, FileInput
 from data_process import DataProcess
 from config import Config
-
+from model_loader import ModelLoader
 
 
 if __name__ == '__main__':
@@ -15,15 +15,17 @@ if __name__ == '__main__':
         )
     # data_input = SensorInput(frequency=config.frequency, window_size=config.window_size)
     data_process = DataProcess(config=config, data_input=data_input)
-    # model = ModelLoader()
+    model = ModelLoader(config.model)
     
     
     while 1:           
         window = data_process.get_next()    
         if window is None:
             break
-        print(window)
-        # model.set_input_window(window)
-        # output_state = model.get_output_state()
-        # led_control.setState(output_state)
+        #print(window)
+          
+        model.set_input_window(window)
+        output_state = model.get_output_state()
+        print(output_state)
+        #led_control.setState(output_state)
         
