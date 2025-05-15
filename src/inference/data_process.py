36bf6a816fdd314b381e4b9fa878ec938cb2842b
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-15 09:26:44 +0200

    Reshuffle

--- src/inference/data_process.py
+++ src/inference/data_process.py
@@ -1,5 +1,5 @@
 import numpy as np
-from feature_extraction import extract_features
+from feature_extraction import extract_features, bandpass_filter
 from collections import deque
 
 
@@ -13,9 +13,9 @@ class DataProcess:
         self.config = config
         self.data_input = data_input
         self.buffer = deque(maxlen=config.window_overlap)
-        self.step = config.window_size // config.window_overlap      
-        
-        
+        self.step = config.window_size // config.window_overlap
+
+
     def _filter_window(self, window):
         return bandpass_filter(
             signal=window,
@@ -24,12 +24,12 @@ class DataProcess:
             fs=self.config.sampling_rate,
             order=self.config.filter_order
         )
-        
+
 
     def get_next_window(self):
         if (self.buffer.count == self.buffer.maxlen):
-            self.buffer.popleft()    
-        
+            self.buffer.popleft()
+
         # Need multiple windows due to window overlap
         while (len(self.buffer) < self.config.pre_buffer_count):
             if (self.data_input.has_next()):  # non-blocking, busy-wait
@@ -37,16 +37,14 @@ class DataProcess:
                 self.buffer.append(next_window)  # TODO: Decide where to filter, here or in process_window?
             else:
                 return None
-        
-        window = np.array(self.buffer).flatten()[self.step:self.step + self.config.window_size]
-        processed_window = self._process_window(window) 
-        
-        self.buffer.popleft()    
 
-        return processed_window  
-            
-            
+        window = np.array(self.buffer).flatten()[self.step:self.step + self.config.window_size]
+        processed_window = self._process_window(window)
+
+        self.buffer.popleft()
+
+        return processed_window
+
+
     def _process_window(self, window):
         return window  # TODO: Add processing
-
-       
