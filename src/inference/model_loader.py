eee5d303a1481115bd2f4c3153eb79b418b9f2ae
CEN90 <77618996+CEN90@users.noreply.github.com>
2025-05-21 11:34:51 +0200

    Mupp

--- src/inference/model_loader.py
+++ src/inference/model_loader.py
@@ -6,8 +6,8 @@ import time
 
 class Model:
     def __init__(self, model_path, logger=None):
-        #self.interpreter = tflite.Interpreter(model_path=model_path)
-        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter = tflite.Interpreter(model_path=model_path)
+        # self.interpreter = tf.lite.Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
